1a588c4615a6a3cfbbe84d91aae88f2622b84443
hardik.bhut <hardik.bhut@rapidops.com>
2026-09-10 15:20:00 +0530

    Updated analytics version to v2

--- build/services/analytics-service.d.ts
+++ build/services/analytics-service.d.ts
@@ -6,6 +6,8 @@ export declare class AnalyticsService {
     static getDeviceId(): any;
     static login(email: any, sourceType?: string, loginMethod?: string, custom_ga4_data?: any): Promise<void>;
     static logout(): Promise<void>;
+    static initUserDetails(): void;
+    static initAnalyticsSession(): void;
     static updateUserDetails(userDetails: any): Promise<boolean>;
     static trackPageView({ pageTitle, pageUrl, experiments }: {
         pageTitle: any;
@@ -35,8 +37,8 @@ export declare class AnalyticsService {
     static trackAcImpressionZero({ noOfResults, acSource, searchTerm, }: any): Promise<void>;
     static trackAcClick({ usedSuggestion, acSource, searchTerm }: any): Promise<void>;
     static trackWidgetViewed({ noOfResults, products_detail, sku, algorithm, rule, rule_type, widget_id, context_type, context_data, category, variant, pageType, pageMetaId, pageDisplayName }: any): Promise<void>;
-    static trackProductViewed({ sku, mode, searchTerm, search_location, category, collection, price, name, brand, productCategories, is_primary_algorithm, is_secondary_algorithm, algorithm, is_merchandising, rule, rule_type, widget_id, context_type, context_data, variant, rules, mode_details, product_option, facets, custom_ga4_data, experiments }: any): Promise<void>;
-    static trackProductVarientViewed({ sku, variant_sku, mode, searchTerm, search_location, category, collection, price, name, brand, productCategories, is_primary_algorithm, is_secondary_algorithm, algorithm, is_merchandising, rule, rule_type, widget_id, context_type, context_data, variant, rules, mode_details, product_option, facets, experiments }: any): Promise<void>;
+    static trackProductViewed({ sku, mode, searchTerm, search_location, category, collection, price, name, brand, productCategories, is_primary_algorithm, is_secondary_algorithm, algorithm, is_merchandising, rule, rule_type, widget_id, context_type, context_data, variant, rules, mode_details, facets, custom_ga4_data, experiments }: any): Promise<void>;
+    static trackProductVarientViewed({ sku, variant_sku, mode, searchTerm, search_location, category, collection, price, name, brand, productCategories, is_primary_algorithm, is_secondary_algorithm, algorithm, is_merchandising, rule, rule_type, widget_id, context_type, context_data, variant, rules, mode_details, facets, experiments }: any): Promise<void>;
     static trackCategoryViewed({ categoryName, items, categoryId, provider_id, sku, products_detail, used_suggestion, search_source, facets, custom_ga4_data, experiments }: {
         categoryName: string;
         items: any;
@@ -62,8 +64,9 @@ export declare class AnalyticsService {
         custom_ga4_data?: any;
         experiments?: any;
     }): Promise<void>;
-    static trackProductAddedToCart({ sku, variantSku, mode, searchTerm, search_location, category, collection, totalValue, quantity, price, name, brand, productCategories, is_primary_algorithm, is_secondary_algorithm, algorithm, is_merchandising, rule, rule_type, widget_id, context_type, context_data, variant, rules, mode_details, product_option, facets, custom_ga4_data, experiments }: any): Promise<void>;
-    static trackProductRemovedFromCart({ sku, name, mode, brand, price, totalValue, quantity, productCategories, variant_sku, searchTerm, category, collection, search_location, is_primary_algorithm, is_secondary_algorithm, algorithm, is_merchandising, rule, rule_type, widget_id, context_type, context_data, variant, rules, mode_details, product_option, facets, custom_ga4_data, experiments }: any): Promise<void>;
+    static trackProductAddedToCart({ sku, variantSku, mode, searchTerm, search_location, category, collection, totalValue, quantity, price, name, brand, productCategories, is_primary_algorithm, is_secondary_algorithm, algorithm, is_merchandising, rule, rule_type, widget_id, context_type, context_data, variant, rules, mode_details, facets, custom_ga4_data, experiments }: any): Promise<void>;
+    static trackProductRemovedFromCart({ sku, name, mode, brand, price, totalValue, quantity, productCategories, variant_sku, searchTerm, category, collection, search_location, is_primary_algorithm, is_secondary_algorithm, algorithm, is_merchandising, rule, rule_type, widget_id, context_type, context_data, variant, rules, mode_details, facets, custom_ga4_data, experiments }: any): Promise<void>;
+    static sanitizeProducts(products: any): any;
     static trackCartViewed({ totalValue, totalQuantity, cartId, baseAmount, cartAmount, discountAmount, items, products, custom_ga4_data, experiments }: any): Promise<void>;
     static trackCheckoutInitiated({ items, totalValue, cartId, totalQuantity, products, custom_ga4_data, experiments }: any): Promise<void>;
     static trackCheckoutCompleted({ items, totalValue, cartId, orderId, totalQuantity, subtotal_tax, currency_code, base_handling_cost, base_shipping_cost, discount_amount, handling_cost_ex_tax, shipping_cost_ex_tax, subtotal_ex_tax, total_ex_tax, wrapping_cost_ex_tax, payment_method, shipping_method, products, custom_ga4_data, experiments }: any): Promise<void>;
